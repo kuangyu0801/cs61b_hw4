a5e9d0fe55e54f972cf78eb80eb5521734e87d40
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-04-10 13:02:53 +0200

    Add files via upload

---
+++ hw4/hw4/puzzle/Board.java
@@ -0,0 +1,169 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.Queue;
+
+public class Board implements WorldState{
+    private int[] board;
+    private int size;
+    private static final int BLANK = 0;
+    private static final boolean useManhattanDistance = true;
+
+    /** Constructs a board from an N-by-N array of tiles where
+    tiles[i][j] = tile at row i, column j*/
+    public Board(int[][] tiles) {
+        size = tiles.length;
+        board = new int[size * size];
+        for (int i = 0; i < size; i += 1) {
+            for (int j = 0; j < size; j += 1) {
+                board[board2Dto1D(i, j)] = tiles[i][j];
+            }
+        }
+    }
+
+    private int board2Dto1D(int i, int j) {
+        return i * size + j;
+    }
+
+    /** Returns the string representation of the board. 
+      * Uncomment this method. */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        int N = size();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", tileAt(i,j)));
+            }
+            s.append("\n");
+        }
+        s.append("\n");
+        return s.toString();
+    }
+
+    /** Returns value of tile at row i, column j (or 0 if blank)*/
+    public int tileAt(int i, int j) {
+        boolean isIOut =  (i > (size - 1) || i < 0) ? true: false;
+        boolean isJOut =  (j > (size - 1) || j < 0) ? true: false;
+        if (isIOut || isJOut) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return board[board2Dto1D(i, j)];
+    }
+
+    /** Returns the board size N*/
+    public int size() {
+        return size;
+    }
+
+    /** return the hamming distance*/
+    public int hamming() {
+        int hammDist = 0;
+        // only item that is not BLANK
+        for (int i = 0; i < (size * size); i += 1) {
+            if (board[i] != i + 1 && board[i] != BLANK) {
+                hammDist += 1;
+            }
+        }
+        return hammDist;
+    }
+
+    private int manhattanCalulator() {
+        return 0;
+    }
+
+    /** return the manhattan distance*/
+    public int manhattan() {
+        int manDist = 0;
+
+        for (int i = 0; i < (size * size); i += 1) {
+            if (board[i] != i + 1 && board[i] != BLANK) {
+                int xDiff = Math.abs(board[i] - (i + 1)) % size;
+                int yDiff = Math.abs(board[i] - (i + 1)) / size;
+                manDist += xDiff + yDiff;
+            }
+        }
+        return manDist;
+    }
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        if (useManhattanDistance) {
+            return manhattan();
+        } else {
+            return hamming();
+        }
+    }
+
+    /** source code provided in http://joshh.ug/neighbors.html*/
+    @Override
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int nextSize = size();
+        int nextX = -1;
+        int nextY = -1;
+        for (int i = 0; i < nextSize; i++) {
+            for (int j = 0; j < nextSize; j++) {
+                if (tileAt(i, j) == BLANK) {
+                    nextX = i;
+                    nextY = j;
+                }
+            }
+        }
+        int[][] nextBoard = new int[nextSize][nextSize];
+        for (int pug = 0; pug < nextSize; pug++) {
+            for (int yug = 0; yug < nextSize; yug++) {
+                nextBoard[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int i = 0; i < nextSize; i++) {
+            for (int j = 0; j < nextSize; j++) {
+                if (Math.abs(-nextX + i) + Math.abs(j - nextY) - 1 == 0) {
+                    nextBoard[nextX][nextY] = nextBoard[i][j];
+                    nextBoard[i][j] = BLANK;
+                    Board neighbor = new Board(nextBoard);
+                    neighbors.enqueue(neighbor);
+                    nextBoard[i][j] = nextBoard[nextX][nextY];
+                    nextBoard[nextX][nextY] = BLANK;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public int[] returnArray() {
+        return board;
+    }
+
+    /** Returns true if this board's tile values are the same
+    position as y's*/
+    @Override
+    public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
+        if (y == null || getClass() != y.getClass()) {
+            return false;
+        }
+
+        Board board1 = (Board) y;
+
+        if (size() != board1.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < size; i += 1) {
+                for (int j = 0; j < size; j += 1) {
+                    if (this.tileAt(i, j) != board1.tileAt(i, j)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = board != null ? board.hashCode() : 0;
+        return result;
+    }
+}
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,178 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Solver {
+    /** Constructor which solves the puzzle, computing
+     * everything necessary for moves() and solution() to
+     * not have to solve the problem again. Solves the
+     * puzzle using the A* algorithm. Assumes a solution exists.*/
+    private int solutionMoves;
+    private ArrayList<WorldState> solutionWorldState;
+    private SearchNode headNode;
+    private SearchNode goalNode;
+    private WorldState startState;
+    private MinPQ<SearchNode> nodePQ;
+    private int enqueueNum; // used for debugging
+    private static final String DEBUGTAG = "[DBG]";
+
+    /**
+     * Constructor
+     * */
+    public Solver(WorldState startState) {
+        solutionWorldState = new ArrayList();
+        Comparator mySearchNodeComparator = new SearchNodeComparator();
+        nodePQ = new MinPQ<SearchNode>(mySearchNodeComparator);
+        this.startState = startState;
+        solutionMoves = 0;
+        enqueueNum = 0;
+        headNode = new SearchNode(startState, 0, null, 0);
+        nodePQ.insert(headNode);
+        solve();
+        System.out.println(DEBUGTAG + "Total enqueued nodes:" + enqueueNum);
+    }
+
+    private void solutionHelper() {
+        solutionWorldState.add(goalNode.nodeState);
+        SearchNode parentNode = goalNode.prevNode;
+        while (!parentNode.nodeState.equals(startState)) {
+            goalNode = goalNode.prevNode;
+            parentNode = goalNode.prevNode;
+            solutionWorldState.add(goalNode.nodeState);
+        }
+        solutionWorldState.add(startState);
+        Collections.reverse(solutionWorldState);
+    }
+
+    private int countMoves(SearchNode inNode) {
+        int countMoves = 0;
+        if (inNode.nodeState.equals(startState)) {
+            return countMoves;
+        }
+
+        countMoves = 1;
+        SearchNode parentNode = inNode.prevNode;
+        while (!parentNode.nodeState.equals(startState)) {
+            inNode = inNode.prevNode;
+            parentNode = inNode.prevNode;
+            countMoves += 1;
+        }
+
+        return countMoves;
+    }
+
+    private void solve() {
+        if (startState.isGoal()) {
+            solutionWorldState.add(startState);
+            return;
+        }
+        while (nodePQ.size() != 0) {
+            /**
+             * Remove the search node with minimum priority.
+             * If it is the goal node, then we’re done.
+             * */
+            SearchNode minNode = nodePQ.delMin();
+            WorldState curState =  minNode.nodeState;
+            int curMoves = minNode.nodeMoves;
+            //System.out.println("[Dequeue] (" + curState + ", " + minNode.nodeMoves + ")");
+            //System.out.println("[Current Moves] " + solutionMoves);
+
+            // if this is a startState, then it doesn't has parent and grandparet
+            // thus setting it to itself
+            WorldState grandParentState = (curState.equals(startState)) ? curState : minNode.prevNode.nodeState;
+            if (curState.isGoal()) {
+                goalNode = minNode;
+                solutionMoves = countMoves(goalNode);
+                solutionHelper();
+                return;
+            }
+
+            Iterable<WorldState> curNeighbors = curState.neighbors();
+
+            /**
+             *  for each neighbor of X’s world state,
+             *  create a new search node that obeys the description above and
+             *  insert it into the priority queue.
+             * */
+            for (WorldState ws: curNeighbors) {
+                /** To avoid enqueuing its own grandparent*/
+                boolean isNoDuplicate = !(ws.equals(grandParentState));
+                if (isNoDuplicate) {
+                    int priority = curMoves + ws.estimatedDistanceToGoal();
+                    nodePQ.insert(new SearchNode(ws, priority, minNode, curMoves + 1));
+                    enqueueNum += 1;
+                    //System.out.println(DEBUGTAG + "[Enqueue] (" + ws + ", " + priority + ", " + minNode.nodeState + ")");
+                    //System.out.println(DEBUGTAG + "[Priority calculation] (" + priority + ", " +solutionMoves + ", " + ws.estimatedDistanceToGoal() + ")");
+                }
+            }
+        }
+    }
+
+    /**
+     * declaring a Comparator for minPQ
+     * */
+    public static class SearchNodeComparator implements Comparator<SearchNode> {
+
+        public SearchNodeComparator() {
+
+        }
+
+        @Override
+        public int compare(SearchNode nodeA, SearchNode nodeB) {
+            return nodeA.compareTo(nodeB);
+        }
+    }
+
+    /**
+     * making SearchNode comparable, is another way to avoid declaring Comparator
+     * */
+    private class SearchNode implements Comparable<SearchNode> {
+        private WorldState nodeState;
+        private int nodeMoves;
+        private SearchNode prevNode;
+        private int priority;
+
+        private SearchNode(WorldState nodeState, int nodePriority, SearchNode prevNode, int nodeMoves) {
+            this.nodeState = nodeState;
+            this.nodeMoves = nodeMoves;
+            this.prevNode = prevNode;
+            this.priority = nodePriority;
+        }
+
+        @Override
+        public int compareTo(SearchNode otherNode) {
+            if (this.priority > otherNode.priority) {
+                return 1;
+            } else if (this.priority < otherNode.priority) {
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    /** Returns the minimum number of moves to solve the puzzle starting
+     *  at the initial WorldState.*/
+    public int moves() {
+        //printSolution();
+        return solutionMoves;
+    }
+
+    /** Returns a sequence of WorldStates from the initial WorldState
+     *  to the solution.*/
+    public Iterable<WorldState> solution() {
+        //printSolution();
+        return solutionWorldState;
+    }
+
+    private void printSolution() {
+        System.out.println("[Solution]: ");
+        for (WorldState ws: solutionWorldState) {
+            System.out.print(ws + ", ");
+        }
+        System.out.println();
+    }
+}
